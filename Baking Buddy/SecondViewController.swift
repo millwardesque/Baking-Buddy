da5550a599d1d490a1bc70d6669950728e77fe48
Christopher Millward <christopher.millward@gmail.com>
2015-12-14 06:50:41 -0500

    Initial Commit

---
+++ Baking Buddy/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Baking Buddy
+//
+//  Created by Chris Millward on 2015-12-14.
+//  Copyright © 2015 Pixel Pistol Games. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

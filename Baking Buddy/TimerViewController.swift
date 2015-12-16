fc74ba779c3572fc870b8ef30def619b8b0604d0
Christopher Millward <christopher.millward@gmail.com>
2015-12-15 23:22:25 -0500

    Added mostly-working timer.

--- Baking Buddy/TimerViewController.swift
+++ Baking Buddy/TimerViewController.swift
@@ -14,10 +14,14 @@ class TimerViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
 
+    var timer = NSTimer()
+    var secondsRemaining = 0.0
+    var hasStarted = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        updateTimeLabel()
+        updateTimeLabel(datePicker.countDownDuration)
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,18 +30,55 @@ class TimerViewController: UIViewController {
     }
     
     @IBAction func onStartButtonPressed(sender: UIButton) {
-        updateTimeLabel()
-        
+        if (!hasStarted) {
+            startTimer()
+        }
+        else {
+            pauseTimer()
+        }
     }
     
     @IBAction func onResetButtonPressed(sender: UIButton) {
-        updateTimeLabel()
+        updateTimeLabel(datePicker.countDownDuration)
     }
     
-    func updateTimeLabel() {
-        let formatter = NSDateFormatter()
-        formatter.setLocalizedDateFormatFromTemplate("hh:mm:ss");
-        timeRemaining.text = formatter.stringFromDate(datePicker.date);
+    func startTimer() {
+        if (!hasStarted) {
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector: Selector("updateTimer"), userInfo: nil, repeats: true)
+            
+            secondsRemaining = datePicker.countDownDuration
+            updateTimeLabel(secondsRemaining)
+            startButton.setTitle("Pause", forState: .Normal)
+            hasStarted = true
+        }
+    }
+    
+    func pauseTimer() {
+        if (hasStarted) {
+            timer.invalidate()
+            startButton.setTitle("Start", forState: .Normal)
+            hasStarted = false
+        }
+    }
+    
+    func updateTimeLabel(secondsRemaining: NSTimeInterval) {
+        let (hours, minutes, seconds) = secondsToHoursMinutesSeconds(Int(secondsRemaining))
+        timeRemaining.text = NSString(format: "%0.2d:%0.2d:%0.2d", hours, minutes, seconds) as String
+    }
+    
+    func updateTimer() {
+        secondsRemaining -= 1.0
+        if (secondsRemaining <= 0.0) {
+            secondsRemaining = 0.0
+            timer.invalidate()
+            hasStarted = false
+            // @TODO Trigger Alarm
+        }
+        
+        updateTimeLabel(secondsRemaining)
+    }
+    
+    func secondsToHoursMinutesSeconds (seconds : Int) -> (Int, Int, Int) {
+        return (seconds / 3600, (seconds % 3600) / 60, seconds % 60)
     }
 }
-
